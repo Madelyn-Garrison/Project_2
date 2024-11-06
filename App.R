47500db288eab349747c9a50f3cf72f30ebd2afa
Madelyn-Garrison <madelynegarrison@gmail.com>
2024-11-06 00:02:13 -0500

    New

--- App.R
+++ App.R
@@ -38,17 +38,6 @@ Holidayvals<-c("1"="No Holiday", '0'="Holiday")
 Hourvals<-c('0',"1","2","3","4","5","6","7","8","9","10","11","12","13","14",
             "15", "16", "17", "18", "19", "20", "21", "22", "23")
 
-
-
-
-
-
-
-
-
-
-
-
 ui <- fluidPage(
   
   titlePanel("Seoul Bike Sharing Exploration"),
